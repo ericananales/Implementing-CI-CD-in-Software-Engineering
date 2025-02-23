3d0fdcf422ff01de4859932b15b069b49e50c511
Erica <erica.nanales@aclcbutuan.edu.ph>
2025-02-23 22:46:07 +0800

    Update CI/CD pipeline and add necessary changes

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,9 @@
-import pytest
-from src.app import app
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+from app import app
 
-@pytest.fixture
+
 def client():
     with app.test_client() as client:
         yield client
